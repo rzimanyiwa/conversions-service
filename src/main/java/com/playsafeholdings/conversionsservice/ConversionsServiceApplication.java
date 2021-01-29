0523fc0caa3bd0a34f490b3ce1ee74e2e17d2a13
Roy Zimanyiwa <rzimanyiwa@gmail.com>
2021-01-29 14:52:48 +0200

    Initial commit

---
+++ src/main/java/com/playsafeholdings/conversionsservice/ConversionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.playsafeholdings.conversionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversionsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playsafeholdings/conversionsservice/ConversionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafeholdings.conversionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
